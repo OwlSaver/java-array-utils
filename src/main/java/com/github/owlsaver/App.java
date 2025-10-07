2efc597001fbf1ae718a98d7089866116a33c581
owlsaver <michaelwacey@msn.com>
2025-10-07 10:55:48 -0400

    Refactor App class formatting for consistency

--- src/main/java/com/github/owlsaver/App.java
+++ src/main/java/com/github/owlsaver/App.java
@@ -4,10 +4,8 @@ package com.github.owlsaver;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
